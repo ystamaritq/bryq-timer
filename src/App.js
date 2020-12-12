c1a0c26394bf0cd97e49ac0f377b8cbf86eb00ce
Yadira Tamarit <ystamaritq@gmail.com>
2020-12-12 11:09:38 -0600

    Update app to use Timer component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Timer from "./components/Timer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [showMessage, setShowMessage] = useState(false);
+	return (
+		<div className="App">
+			<Timer
+				value={4}
+				onExpire={() => setShowMessage(true)}
+			/>
+      {showMessage && <h3 style={{paddingLeft: 35}}>The timer has expired!</h3>}
+		</div>
+	);
 }
 
 export default App;
