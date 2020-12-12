83d4a097295efc6a3567ce0f648b8c50b45ff6b8
Yadira Tamarit <ystamaritq@gmail.com>
2020-12-12 13:32:17 -0600

    Update App test

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ const App = () => {
   const [showMessage, setShowMessage] = useState(false);
 	return (
 		<div className="App">
-			<Timer
-				value={4}
+			<Timer data-testid="timer-component"
+				value={60}
 				onExpire={() => setShowMessage(true)}
 			/>
       {showMessage && <h3 style={{paddingLeft: 35}}>The timer has expired!</h3>}
